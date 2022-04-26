96a935fc283ef235ce189ff82a7de32a5b716171
Oleksii Kikot <oleksii.kikot@yahoo.com>
2022-04-26 19:48:24 +0200

    completed third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onClick);
+
+function onClick(e) {
+  e.preventDefault();
+
+  const DELAY = Number(form.elements.delay.value);
+  const STEP = Number(form.elements.step.value);
+  const AMOUNT = Number(form.elements.amount.value);
+
+  let counter = 1;
+
+  for (let i = 0; i < AMOUNT; i += 1) {
+    createPromise(counter, DELAY + i * STEP)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    counter += 1;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
